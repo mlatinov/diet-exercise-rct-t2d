3ab338efced24aa00bd6afa5d2a4b76cf1caa1e5
mlatinov <latinovmetodi@gmail.com>
2026-05-15 21:15:09 +0300

    tune the dgp to account for the added model interaction

--- dag/dgp_anthropometry.R
+++ dag/dgp_anthropometry.R
@@ -3,27 +3,39 @@
 
 ## Total effect of PIP on post-intervention BMI
 dgp_total_bmi <- function(
-  n,
-  treatment_p = 0.5,
-  bmi_pre_mu = 30, bmi_pre_sd = 5, bmi_post_sd = 2,
-  beta_treatment = -2, beta_bmi_pre = 0.95, beta_0 = 0
+    n,
+    treatment_p          = 0.5,
+    bmi_pre_mu           = 30,
+    bmi_pre_sd           = 5,
+    beta_0               = 0,
+    beta_treatment       = -2,
+    beta_bmi_pre         = 0.95,
+    treatment_bmi_pre    = 0.3,
+    sigma_intercept      = log(3),   
+    beta_sigma_treatment = 0         
 ) {
-  # Simulate Treatment
+  # Simulate Treatment ,BMI Pre, 
   treatment <- rbinom(n = n, size = 1, prob = treatment_p)
-
-  # BMI Pre
   bmi_pre <- rnorm(n = n, mean = bmi_pre_mu, sd = bmi_pre_sd)
-
-  # BMI Post
-  bmi_post_mu <- beta_0 + beta_treatment * treatment + beta_bmi_pre * bmi_pre
-
+  
+  # Center the Bmi pre
+  bmi_pre_c <- bmi_pre - mean(bmi_pre)
+  
+  # Mean Submodel 
+  bmi_post_mu <- beta_0 + beta_treatment * treatment + beta_bmi_pre * bmi_pre_c +
+    treatment_bmi_pre * treatment * bmi_pre_c
+  
+  # Sigma Submodel 
+  log_sigma <- sigma_intercept + beta_sigma_treatment * treatment
+  sigma     <- exp(log_sigma)
+  
   # Sample
-  bmi_post <- rnorm(n = n, mean = bmi_post_mu, sd = bmi_post_sd)
+  bmi_post <- rnorm(n = n, mean = bmi_post_mu, sd = sigma)
 
   # Combine and return a dataframe
   data <- data.frame(
     treatment = treatment,
-    bmi_pre = bmi_pre,
+    bmi_pre = bmi_pre_c,
     bmi_post = bmi_post
   )
   return(data)
